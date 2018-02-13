b5ce527bb3a1ca941241200e0a5529aa9b049a92
Carlos Felipe Agudelo Ospina <cf.agudelo96@gmail.com>
2018-02-13 15:00:42 -0500

    Correcciones

---
+++ app/models/User.java
@@ -0,0 +1,12 @@
+package models;
+
+import java.util.*;
+import javax.persistence.*;
+
+import io.ebean.*;
+import play.data.format.*;
+import play.data.validation.*;
+
+@Entity
+public class User extends Model {
+}
